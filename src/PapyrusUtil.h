e0e32bfef89254a27108b66668b959f2153deda4
maniman303 <maniman303@gmail.com>
2025-11-17 08:52:45 +0100

    Upgrade commonlibf4 to fix AE game update

--- src/PapyrusUtil.h
+++ src/PapyrusUtil.h
@@ -5,11 +5,5 @@
 
 namespace PapyrusUtil
 {
-	std::vector<int> GetArmorBipedSlots(std::monostate, RE::TESObjectARMO* armor);
-
-	void LogScript(std::monostate, RE::BSFixedString message);
-
-	void Notification(std::monostate, RE::BSFixedString message);
-
 	bool BindFunctions(RE::BSScript::IVirtualMachine* vm);
 }
